02a60e922e426981ccf8674d050b445c1d62132a
sukjun2 <tjrwns3650@gmail.com>
2022-11-10 16:42:10 +0900

    22.11.10

--- src/components/include/MovieList.jsx
+++ src/components/include/MovieList.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/free-mode";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import { Autoplay, FreeMode, Pagination } from "swiper";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper";
 
 function MoviePop(props) {
     return (
-            <li>
-                <a href={`https://www.themoviedb.org/movie/${props.movie.id}`} target="_blank" rel="noreferrer">
-                    <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt={props.movie.title} className="movie_img" />
-                    <em>
-                        <span className="title">{props.movie.title}</span>
-                        <span className="star">{props.movie.vote_average}</span>
-                        <span className="rank">{props.index + 1}</span>
-                    </em>
-                </a>
-            </li>
+        <li>
+            <a href={`https://www.themoviedb.org/movie/${props.movie.id}`} target="_blank" rel="noreferrer">
+                <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt={props.movie.title} className="movie_img" />
+                <em>
+                    <span className="title">{props.movie.title}</span>
+                    <span className="star">{props.movie.vote_average}</span>
+                    <span className="rank">{props.index + 1}</span>
+                </em>
+            </a>
+        </li>
     );
 }
 
@@ -26,28 +26,33 @@ const MovieList = (props) => {
             <div className="container">
                 <div className="list__inner">
                     <h2>Popular Movies</h2>
-                    <ul>
-                        <Swiper
-                            slidesPerView={5}
-                            spaceBetween={30}
-                            freeMode={true}
-                            autoplay={{
-                                delay: 2000,
-                                disableOnInteraction: false,
-                              }}
-                            pagination={{
-                            clickable: true,
-                            }}
-                            modules={[Autoplay, FreeMode, Pagination]}
-                            className="mySwiper"
-                        >
+                    <Swiper
+                        effect={"coverflow"}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={"auto"}
+                        initialSlide="3"
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: false,
+                        }}
+                        pagination={false}
+                        modules={[Autoplay, EffectCoverflow, Pagination]}
+                        className="mySwiper"
+                    >
                         {props.lists.map((movies, index) => (
                             <SwiperSlide key={index}>
                                 <MoviePop key={index} movie={movies} index={index} />
                             </SwiperSlide>
                         ))}
                     </Swiper>
-                    </ul>
                 </div>
             </div>
         </section>
